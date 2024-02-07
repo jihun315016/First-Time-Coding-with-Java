dbfbe5b0ea57d8a7a4120ea074b6e98743b97b58
jihun <jihun315016@gmail.com>
2024-02-07 16:14:38 +0900

    java init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# First-Time-Coding-with-Java
-코딩은 처음이라 with 자바
+# JavaStudy
+Java 언어 관련 공부한 것
--- ch01_HelloWorld/src/com/yudong80/java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.yudong80.java;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");        
-    }
-}
