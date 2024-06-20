11c221df906fb61b8b4fd6b8072e667a9a42a0d2
jihun <jihun315016@gmail.com>
2024-06-20 21:03:50 +0900

    스프링 jpa 업데이트

--- shop/src/main/java/com/example/shop/ItemController.java
+++ shop/src/main/java/com/example/shop/ItemController.java
@@ -1,13 +1,13 @@
 package com.example.shop;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -17,7 +17,8 @@ import java.util.Optional;
 public class ItemController {
 
     private final ItemRepository itemRepository;
-    
+    private final ItemService itemService;
+
 //    @Autowired
 //    public ItemController(ItemRepository itemRepository) {
 //        this.itemRepository = itemRepository;
@@ -43,29 +44,75 @@ public class ItemController {
     // url 작명은 동사보다는 명사로 하는 게 좋음
     // @RequestParam 생략 가능
     @PostMapping("/add")
-    // String writePost(@RequestParam String title, @RequestParam Integer price)
-    String writePost(@ModelAttribute Item item) {
-        System.out.println(item);
-        itemRepository.save(item);
+    String writePost(@RequestParam String title, @RequestParam Integer price) {
+    // String writePost(@ModelAttribute Item item) {
+        itemService.saveItem(title, price);
         return "redirect:/list";
     }
 
     // id를 url 파라미터라고 부름
     @GetMapping("/detail/{id}")
-    String detail(@PathVariable Long id, Model model) {
+    //ResponseEntity<String> detail(@PathVariable Long id, Model model) {
+    String detail(@PathVariable Long id, Model model) throws Exception {
         Optional<Item> result = itemRepository.findById(id);
         // Optional 타임은 이 변수가 비어있을 수도 있고 Item 타입일 수도 있다는 뜻
         // 결과가 없는 경우를 대비하기 위한 타입
         // 이 변수의 값을 쓸 때는 .get()을 써야 함
+
+        //throw new Exception();
+
+//        try {
+//            // throw new Exception("이런 저런 이유임");
+//            if (result.isPresent()) {
+//                // result 값 없는데 get하면 오류 남
+//                System.out.println(result.get());
+//                model.addAttribute("item", result.get());
+//                return "detail.html";
+//            } else {
+//                return "redirect:/list";
+//                // 에러 코드 -> 에러 원인을 알려주는 코드
+//                //return ResponseEntity.status(HttpStatus.NOT_FOUND).body("에러 메시지");
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            return "redirect:/list";
+//        }
+        return "redirect:/list";
+    }
+
+    @GetMapping("/edit/{id}")
+        //ResponseEntity<String> detail(@PathVariable Long id, Model model) {
+    String edit(Model model, @PathVariable Long id) {
+        Optional<Item> result = itemRepository.findById(id);
         if (result.isPresent()) {
-            // result 값 없는데 get하면 오류 남
-            System.out.println(result.get());
-            model.addAttribute("item", result.get());
+            model.addAttribute("data", result.get());
+            return "edit.html";
         } else {
             return "redirect:/list";
         }
-        return "detail.html";
     }
+
+    @PostMapping("/edit")
+        //ResponseEntity<String> detail(@PathVariable Long id, Model model) {
+    String editItem(@RequestParam String title, @RequestParam Integer price, Long id) {
+        Item item = new Item();
+        item.setId(id);
+        item.setTitle(title);
+        item.setPrice(price);
+        
+        // id가 1인 행이 있으니까 덮어쓰기(수정) -> jpa로 수정하는 방법
+        itemRepository.save(item);
+        return "redirect:/list";
+    }
+
+//    @ExceptionHandler(Exception.class)
+//    public void handler() {
+//        // 같은 클래스의 메서드 안에서 오류가 나면 대신 이 안에 있는 코드를 실행해 줌
+//        // try catch를 써도 파라미터 잘못 입력되거나 하는건 못 잡음
+//        // return ResponseEntity.status().body();
+//    }
+    // 근데 이것도 컨트롤러 많아지면 귀찮은데
+    // 모든 컨트롤러 파일을 에러를 캐치하려면 @ControllerAdvice -> MyExceptionHandler 클래스 참고
 }
 // jpa 사용하기
 // 1. repository 만들기
