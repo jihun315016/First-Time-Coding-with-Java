4a80c75e5799b8ab8e1031484a4a16d3545019f7
jihun <jihun315016@gmail.com>
2024-06-22 17:47:05 +0900

    youtube data api 연동 테스트

---
+++ spring-test/src/main/java/com/example/spring_test/youtube/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.spring_test.youtube;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
--- spring-test/src/test/java/com/example/spring_test/SpringTestApplicationTests.java
+++ spring-test/src/test/java/com/example/spring_test/SpringTestApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springboot_dev;
+package com.example.spring_test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootDevApplicationTests {
+class SpringTestApplicationTests {
 
 	@Test
 	void contextLoads() {
