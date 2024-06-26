a7265d3b3cd4d6884d6a7bb2152d7b5237103075
jihun <jihun315016@gmail.com>
2024-06-26 23:00:42 +0900

    타임리프 레이아웃

--- spring-test/src/main/java/com/example/spring_test/HomeController.java
+++ spring-test/src/main/java/com/example/spring_test/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.spring_test;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
-    public String index() {
-        return "Hello World";
+    public String home() {
+        return "content";
     }
 }
